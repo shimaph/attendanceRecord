49a4855d81a98cf97d54de232716aeacfbccdc5e
shimaph <63526397+shimaph@users.noreply.github.com>
2021-02-22 08:40:55 +0900

    upgrade

--- index.js
+++ index.js
@@ -1,53 +1,101 @@
-//変数　：書き換え可能
-let dialog = 'Hello World!';
-//dialog = 'Hello World!!';
 
-//定数 ：書き換えができない
-const bigDialog = 'He..Hell...Hello World!';
+let l = localStorage;
+i = 0
+count = 0
+const getStartTime = () => {
+  startTime = new Date(); //関数内でvarを使わないとグローバル変数になる
+  document.getElementById('startTime').innerHTML = startTime.toLocaleString();
+};
 
-//配列
-let chess = ['mma','papa','dlkjf','dslkfj'];
-
-// ループ文
-// let index = 0;
-// while(index < chess.length){
-//   console.log(chess[index]);
-//   index++;
-//}
+const getEndTime = () => {
+  endTime = new Date();
+  document.getElementById('endTime').innerHTML = endTime.toLocaleString();
+};
 
 
+const getDiff = () => {
+  let diff = endTime - startTime;
+  let hours = Math.floor(diff/(1000 * 60 * 60));
+  diff = diff % (1000 * 60 * 60);
+  let mins = Math.floor(diff/(1000 * 60));
 
-
-const test = () => {
-  if(chess.length > 5){
-    console.log('ワッツアップ！！');
+  if(hours != 0){
+    diff = hours + "時間" +　mins + "分";
   } else {
-    console.log('よう！clubhouse!!');
+    diff = mins + "分";
   }
+
+  let startTimeInHM = startTime.getHours() + "時" + startTime.getMinutes() + "分";
+  let endTimeInHM = endTime.getHours() + "時" + endTime.getMinutes() + "分";
+
+  let MM = startTime.getMonth();
+  let DD = startTime.getDate();
+
+  i += 1;
+  l.setItem("workTime" + i.toString(), MM + "/" + DD + " " + startTimeInHM + "-" + endTimeInHM + " " + diff);
+  console.log(l.getItem("workTime"+ i.toString()))
+
+
+  console.log(i);
+  document.getElementById('workedTime').textContent = diff;
 };
 
-test();
-
-//任意のタイミング手使いたい or 繰り返し使いたい！
 
 
-//オブジェクト
-const chess2 = {
-  color: 'pink',
-  size: 'large',
-  purfume: 'mint'
+const getDiffSec = () => {
+  let diffSec = startTime - endTime;
+  console.log(diffSec)
 };
 
-console.log(chess2)
 
-//window.inner
-//window.alert()
-//
-// document.getElementsByTagName('button')[0].addEventListener('click', ()=>{
-//   window.alert("Hello World!");
-// });
+const start = '出勤';
+const end = '＞　退勤';
 
-const start = '出勤'
-const end = '退勤'
+startButton = document.getElementById('startButton');
+endButton = document.getElementById('endButton');
+showButton = document.getElementById('showButton');
 
-document.getElementById('startButton').textContent = start;
+startButton.textContent = start;
+endButton.textContent = end;
+showButton.textContent = "提出";
+
+
+
+startButton.addEventListener('click',()=>{
+  if(count == 0){
+    confirmStart = confirm("作業を開始しますか？");
+    if(confirmStart == true){
+      alert("出勤開始時間を記録しました。")
+      count += 1;
+      getStartTime();
+      startButton.classList.add('duringWork');
+      startButton.textContent = "出勤中"
+    }
+  } else {
+    alert("現在出勤中です。")
+  }
+})
+
+endButton.addEventListener('click',()=>{
+  if (count == 1){
+    confirmEnd = confirm("作業を終了しますか？");
+    if(confirmEnd == true){
+      getEndTime();
+      count -= 1;
+      getDiff();
+      // saveHours();
+    }
+  } else {
+    alert("まず作業を開始してください。")
+  }
+})
+
+showButton.addEventListener('click', () => {
+ for(; i > 0; i--){
+   let div = document.createElement('div');
+   div.textContent = l.getItem("workTime"+ i.toString());
+   document.body.appendChild(div);
+ }
+
+
+});
